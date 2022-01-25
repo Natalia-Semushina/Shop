9ed438f7419576704218f9b0ff7275f54f2d9a42
Natalia Semushina <natusic431ka@rambler.ru>
2022-01-25 21:32:24 +0300

    Toolbar, Tabs and tables without batton Delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,53 @@
 import { Component } from '@angular/core';
 
+export interface TableElement {
+  name: string;
+  position: number;
+  quantity: number;
+  price: number;
+
+}
+
+const VAGETEBLES_DATA: TableElement[] = [
+  {position: 1, name: 'Картошка', quantity: 10, price: 430},
+  {position: 2, name: 'Морковка', quantity: 2, price: 82},
+  {position: 3, name: 'Лук', quantity: 2, price: 46},
+  {position: 4, name: 'Капуста', quantity: 1, price: 70},
+  {position: 5, name: 'Свекла', quantity: 2, price: 96},
+  {position: 6, name: 'Перец', quantity: 2, price: 250},
+  {position: 7, name: 'Помидоры', quantity: 5, price: 400},
+  {position: 8, name: 'Огурцы', quantity: 3, price: 210},
+  {position: 9, name: 'Редиска', quantity: 1, price: 120},
+  {position: 10, name: 'Кабачки', quantity: 1.5, price: 90},
+];
+
+const FRUT_DATA: TableElement[] = [
+  {position: 1, name: 'Яблоки', quantity: 10, price: 430},
+  {position: 2, name: 'Груши', quantity: 2, price: 82},
+  {position: 3, name: 'Сливы', quantity: 2, price: 46},
+  {position: 4, name: 'Виноград', quantity: 1, price: 70},
+  {position: 5, name: 'Персики', quantity: 2, price: 96},
+  {position: 6, name: 'Бананы', quantity: 2, price: 250},
+  {position: 7, name: 'Алыча', quantity: 5, price: 400},
+  {position: 8, name: 'Абрикосы', quantity: 3, price: 210},
+  {position: 9, name: 'Черешня', quantity: 1, price: 120},
+  {position: 10, name: 'Вишня', quantity: 1.5, price: 90},
+];
+
+const MILC_DATA: TableElement[] = [
+  {position: 1, name: 'Молоко', quantity: 10, price: 430},
+  {position: 2, name: 'Кефир', quantity: 2, price: 82},
+  {position: 3, name: 'Ряженка', quantity: 2, price: 46},
+  {position: 4, name: 'Закваска', quantity: 1, price: 70},
+  {position: 5, name: 'Йогурт', quantity: 2, price: 96},
+  {position: 6, name: 'Снежок', quantity: 2, price: 250},
+  {position: 7, name: 'Творог', quantity: 5, price: 400},
+  {position: 8, name: 'Сметана', quantity: 3, price: 210},
+  {position: 9, name: 'Тан', quantity: 1, price: 120},
+  {position: 10, name: 'Айран', quantity: 1.5, price: 90},
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +55,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fix';
+  displayedColumns: string[] = ['position', 'name', 'quantity', 'price'];
+  vegSource = VAGETEBLES_DATA;
+  frutSource = FRUT_DATA;
+  milkSource = MILC_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatTabsModule,
+        MatTableModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
